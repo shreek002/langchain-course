f683bdce0e4a6ed72e86d18fea06425caed972d2
Harsha Kancharla <harsha.kancharla@oracle.com>
2026-04-23 15:24:54 +0530

    Search agent done

--- main.py
+++ main.py
@@ -7,16 +7,17 @@ load_dotenv()
 from langchain.agents import create_agent
 from langchain.tools import tool
 from langchain_core.messages import HumanMessage
-from langchain_openai import ChatOpenAI
+# from langchain_openai import ChatOpenAI
+from langchain_google_genai import ChatGoogleGenerativeAI
 from langchain_tavily import TavilySearch
 
-
+#Pydantic objects which will make the required format for output for json
 class Source(BaseModel):
     """Schema for a source used by the agent"""
 
     url: str = Field(description="The URL of the source")
 
-
+#Pydantic objects which will make the required format for output for json
 class AgentResponse(BaseModel):
     """Schema for agent response with answer and sources"""
 
@@ -26,7 +27,8 @@ class AgentResponse(BaseModel):
     )
 
 
-llm = ChatOpenAI(model="gpt-5")
+# llm = ChatOpenAI(model="gpt-5")
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", temperature=0)
 tools = [TavilySearch()]
 agent = create_agent(model=llm, tools=tools, response_format=AgentResponse)
 
